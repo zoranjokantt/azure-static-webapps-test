836e3ce1cf81416801d586a7b0de627fb1afd76e
Zoran Joka <zoran.joka@gmail.com>
2020-06-22 11:52:39 +0200

    Add first function

---
+++ api/GetMessage/index.ts
@@ -0,0 +1,12 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    context.log('HTTP trigger function processed a request.');
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: "Hello from the API"
+    };
+};
+
+export default httpTrigger;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function App() {
   const value = 'World';
+
+  useEffect(() => {
+    fetch('http://localhost:7071/api/message', (res) => {
+      console.log(res);
+    })
+  }, []);
+
   return <div>Hello {value}</div>;
 }
 
